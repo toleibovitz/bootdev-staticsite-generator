073fbec99bdcf7add889a035f7cd20ce54432a99
toleibovitz <toleibovitz@gmail.com>
2025-10-06 21:13:50 -0500

    logic for github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,7 +3,7 @@ import pathlib
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -19,7 +19,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-
+    template = template.replace('href=/', f'href="{base_path}')
+    template = template.replace('src=/', f'src="{base_path}') 
+    
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
@@ -27,7 +29,7 @@ def generate_page(from_path, template_path, dest_path):
     to_file.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     contents = os.listdir(dir_path_content)
     for filename in contents:
         from_path = os.path.join(dir_path_content, filename)
@@ -35,9 +37,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         print(f"From Path: {from_path}")
         if os.path.isfile(from_path):
             dest_path = pathlib.Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
         
             
 
